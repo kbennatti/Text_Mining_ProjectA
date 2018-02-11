5713185fd715fbc0ead39f3655f03d1883c864a1
sabrinatm <32022289+sabrinatm@users.noreply.github.com>
2018-02-11 18:17:23 -0500

    Add files via upload

--- stream.py
+++ stream.py
@@ -1,39 +1,68 @@
 #!/usr/bin/env python
 # encoding: utf -8
-import tweepy 
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 import json
+from nltk.tokenize import word_tokenize
+import re
 
-# Authentication details. To obtain these visit dev.twitter.com
 access_token = "252197958-qd0oMpuYd0VyB7Ghb0V3wzksS9heZkT3S2GfdSVs"
-access_token_secret = "WK1ZNHalvvPsNVC2ogL5l6ojo8FwtMeXsYIvjWzakb8PA"
+access_secret = "WK1ZNHalvvPsNVC2ogL5l6ojo8FwtMeXsYIvjWzakb8PA"
 consumer_key = "vqusFEWFnnqEwqwXdAJnozuay"
 consumer_secret =   "Dbc4mcepoUGDBuscEtF4YDUYV8MF9wBcUztH5eXI8AOAl6uIVv"
 
-#This is the listener, responsible for recieving data
-class StdOutListener(tweepy.StreamListener):
+auth=OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api=tweepy.API(auth)
+
+#streaming the tweets
+
+class MyListener(StreamListener):
 	def on_data(self,data):
-		#parsing
-		decoded=json.loads(data)
-		#open a file to store the status objects
-		file=open('stream.json', 'wb')
-		#write json to file
-		json.dump(decoded,file,sort_keys = True,indent=4)
-		#show progress
-		print "Writing tweets to file, CTRL + C to terminate the program"
-
+		try:
+			with open('twitter.json','a') as f:
+				f.write(data)
+				return True
+		except BaseException as e:
+			print("Error on_data: %s" % str(e))
 		return True
+
 	def on_error(self,status):
-		print status
-if __name__ =='__main__':
-	l=StdOutListener()
-	auth=tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_token_secret)
-	
-	#there are different kinds of streams
-	stream=tweepy.Stream(auth,l)
-	#Hashtag to stream
-	stream.filter(track=["#cryptocurrency"])
+		print(status)
+		return True
+
+twitter_stream=Stream(auth,MyListener())
+twitter_stream.filter(track=['#cryptonews'])
 
 
 
 
+
+
+
+#terms co-occurence
+
+com=defaultdict(lambda : defaultdict(int))
+with open(fname, 'r') as f:
+	for line in f:
+		tweet=json.loads(line)
+		terms_only=[term for term in preprocess(tweet['text']) 
+				if term not in stop 
+				and not term.startswith(('#','@'))]
+		for i in range(len(terms_only)-1):
+			for j in range(i+1, len(terms_only)):
+				w1,w2=sorted([terms_only[i],terms_only[j]])
+				if w1 != w2:
+					com[w1][w2] +=1
+com_max=[]
+#for each term, look for the most common co-occurences
+for t1 in com:
+	t1_max_terms=sorted(com[t1].items(), key=operator.itemgetter(1), reverse=True)[:5]
+	for t2,t2_count in t1_max_terms:
+		com_max.append(((t1,t2), t2_count))
+#get the most frequent co-occurences
+terms_max=sorted(com_max, key=operator.itemgetter(1),  reverse=True)
+#print(terms_max[:5])
