3f1c78be83cc83586114a5869bcbcb3a6e63b821
kbennatti <32470198+kbennatti@users.noreply.github.com>
2018-03-04 14:19:52 -0500

    added normalization and processing in
    created two different sql tables

--- ccn_to_sql.py
+++ ccn_to_sql.py
@@ -1,6 +1,21 @@
+from __future__ import division
 import pprint
 import sqlite3
 import requests
+import numpy as np
+import pprint
+import sqlite3
+import plotly
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+import math
+import random as rand
+
 
 coinsNewsURL = requests.get(
     "https://newsapi.org/v2/everything?sources=crypto-coins-news&apiKey=1d656ac0916147bf8d28e1dcda71266a")
@@ -30,8 +45,83 @@ for i in range(0, len(articles)):
 # feed from website to sql table
 conn = sqlite3.connect('cryptobase.db')  # connection
 c = conn.cursor()
-#c.execute('SELECT published FROM coinsNews WHERE id = (SELECT max(id) FROM coinsNews)')
-c.execute('DROP TABLE coinsNews')
+c.execute('DROP TABLE IF EXISTS coinsNews')
 c.execute('CREATE TABLE coinsNews(id INTEGER, author TEXT, title TEXT, description TEXT, url TEXT, urlToImage TEXT, published TEXT)')
 c.executemany('INSERT INTO coinsNews VALUES(?,?,?,?,?,?,?)', articlesList)
+conn.commit()
+
+# get table from database
+def tableFromDatabase(database, table):
+    conn = sqlite3.connect(database)  # connection
+    c = conn.cursor()  # get a cursor object, all SQL commands are processed by
+    c.execute('SELECT * FROM %s' % table)
+    tableRows = c.fetchall()
+    return tableRows
+
+rows = tableFromDatabase('cryptobase.db', 'coinsNews')
+
+# pass sql table to python list
+coinsList = [list(row) for row in rows]
+
+# normalize words
+stop_words = set(stopwords.words('english') + ['cryptocurrency', 'cryptocurrencies', 'u', '000'])
+lemmatizer = WordNetLemmatizer()
+ps = PorterStemmer()
+tokenizer = RegexpTokenizer(r'\w+') # takes only alphanum characters
+
+def normalize(table):
+    normal = []
+    counts = []
+    for row in table:
+        word_tokens = tokenizer.tokenize(row[2])
+        word_lower = [w.lower() for w in word_tokens]
+        word_stem = [lemmatizer.lemmatize(w) for w in word_lower]
+        filtered_row = [w for w in word_stem if not w in stop_words]
+        normal.append(filtered_row)
+        counts.append(Counter(filtered_row))
+    return normal
+
+normalized = normalize(coinsList)
+
+def countW(normalized):
+    allWords = []
+    for row in normalized:
+        for word in row:
+            allWords.append(word)
+    return Counter(allWords)
+
+countsWords = countW(normalized)
+
+# calculate tf-idf
+def inverse_freq(all_documents):
+    idf_values = {}
+    all_tokens_set = set([item for row in all_documents for item in row])
+    for tkn in all_tokens_set:
+        contains_token = map(lambda doc: tkn in doc, all_documents)
+        idf_values[tkn] = math.log(len(all_documents) / (sum(contains_token)))
+    return idf_values
+
+#def tfidf(term, all_documents):
+idf = inverse_freq(normalized)
+tfidfs = {}
+for term in idf:
+       tfidf = idf[term]*countsWords[term]
+       tfidfs[term]=tfidf
+
+#sortedTfidfs = sorted(tfidfs.items(), key=lambda x:x[1], reverse = True)
+sortVal = sorted(tfidfs.values(), reverse=True)
+sortKey = sorted(tfidfs, key=tfidfs.get, reverse=True)
+
+valKey = []
+for i in range(len(sortKey)):
+    row=[]
+    row.append(sortKey[i])
+    row.append(sortVal[i])
+    valKey.append(row)
+
+conn = sqlite3.connect('cryptobase.db')  # connection
+c = conn.cursor()
+c.execute('DROP TABLE IF EXISTS sortedTFIDF')
+c.execute('CREATE TABLE sortedTFIDF (word TEXT, value INTEGER)')
+c.executemany('INSERT INTO sortedTFIDF VALUES(?,?)', valKey)
 conn.commit()
